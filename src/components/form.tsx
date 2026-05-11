138eea5b157e071fade9ad0c679d5090371b703b
waledkhaledd205 <waledkhaledd205@gmail.com>
2026-05-11 20:34:17 +0300

    fix

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import SelectCountry from "@/components/selectCountry";
+import SelectCountry from "@/components/Country";
 
 export default function Form({ countries }: { countries: any[] }) {
   const [country, setCountry] = useState("");
